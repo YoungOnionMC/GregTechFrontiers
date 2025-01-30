33377caca29a5de582110b5d394517bdb1a5ab08
Ghostipedia <Ghostipedia@gmail.com>
2025-01-30 11:18:40 -0500

    Fixed the Last Coil Item Render & Aether Portal Frames

---
+++ src/main/java/com/ghostipedia/cosmiccore/common/item/RenderBlockItem.java
@@ -0,0 +1,29 @@
+package com.ghostipedia.cosmiccore.common.item;
+
+import com.lowdragmc.lowdraglib.client.renderer.IBlockRendererProvider;
+import com.lowdragmc.lowdraglib.client.renderer.IItemRendererProvider;
+import com.lowdragmc.lowdraglib.client.renderer.IRenderer;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import org.jetbrains.annotations.Nullable;
+
+public class RenderBlockItem extends BlockItem implements IItemRendererProvider {
+    public RenderBlockItem(Block block, Properties properties) {
+        super(block, properties);
+    }
+
+    @Nullable
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public IRenderer getRenderer(ItemStack stack) {
+        if (getBlock() instanceof IBlockRendererProvider provider) {
+            return provider.getRenderer(getBlock().defaultBlockState());
+        }
+        return null;
+    }
+}
