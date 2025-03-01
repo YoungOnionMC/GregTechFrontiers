fabea7bae6d3c5ab92109975298275cd709d1ca6
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2025-03-01 00:16:09 -0700

    added Ice and Sun fern seeds to composting

---
+++ overrides/kubejs/server_scripts/Recipes/SurvivalOverhaul.js
@@ -0,0 +1,5 @@
+
+ServerEvents.compostableRecipes(event => {
+    event.add(Item.of('legendarysurvivaloverhaul:ice_fern_seeds'), 0.2)
+    event.add(Item.of('legendarysurvivaloverhaul:sun_fern_seeds'), 0.2)
+})
